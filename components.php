b6a36508260a8fd1e1337ccb6f76bdad3a8235be
anna-e-gorova <80091222+anna-e-gorova@users.noreply.github.com>
2022-02-17 18:43:17 +0300

    lesson4

---
+++ components.php
@@ -0,0 +1,63 @@
+<?php
+interface DBConnection 
+{
+
+}
+
+interface DBRecord 
+{
+    
+}
+
+interface DBQueryBuiler 
+{
+    
+}
+
+
+class MySQLConnection implements DBConnection
+{
+
+}
+
+class MySQLRecord implements DBRecord
+{
+
+}
+
+class MySQLQueryBuiler implements DBQueryBuiler
+{
+
+}
+
+
+class PostgreConnection implements DBConnection
+{
+
+}
+
+class PostgreRecord implements DBRecord
+{
+
+}
+
+class PostgreQueryBuiler implements DBQueryBuiler
+{
+
+}
+
+
+class OracleConnection implements DBConnection
+{
+
+}
+
+class OracleRecord implements DBRecord
+{
+
+}
+
+class OracleQueryBuiler implements DBQueryBuiler
+{
+
+}
